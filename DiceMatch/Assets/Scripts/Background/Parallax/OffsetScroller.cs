2728457339c165e04d39416d2066c358697f16f7
NickAk <asoman97@gmail.com>
2018-04-06 22:50:21 +0300

    Initial commit
    Some backgrounds (paralax forest for ex) and one dice. Working on dice grid postitioing

---
+++ DiceMatch/Assets/Scripts/Background/Parallax/OffsetScroller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OffsetScroller : MonoBehaviour
+{
+
+    public float scrollSpeed;
+    private Vector2 savedOffset;
+
+    void Start()
+    {
+        savedOffset = GetComponent<Image>().material.GetTextureOffset("_MainTex");
+        //savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(x, savedOffset.y);
+        //GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+        GetComponent<Image>().material.SetTextureOffset("_MainTex", offset);
+    }
+
+    void OnDisable()
+    {
+        //GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
+        GetComponent<Image>().material.SetTextureOffset("_MainTex", savedOffset);
+    }
+}
